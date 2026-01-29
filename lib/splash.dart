7f359ff7183764107b593e7e598d6a334d580255
Spark16x <spark2009971@gmail.com>
2026-01-29 10:46:18 +0530

    update

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'main.dart'
+import 'main.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
