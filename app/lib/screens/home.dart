6bf7dda0c1bac0576ac5b5fc9197ebd821bc0ecc
spark16x <spark2009971@gmail.com>
2026-02-01 19:55:26 +0530

    update home.dart

--- app/lib/screens/home.dart
+++ app/lib/screens/home.dart
@@ -23,9 +23,9 @@ class _HomeScreenState extends State<HomeScreen> {
           // 🌍 OpenStreetMap Map Widget
           FlutterMap(
             options: MapOptions(
-   initialCenter: _initialPos,
-    zoom: 14,
-),
+              initialCenter: _initialPos,
+              initialZoom: 14,
+            ),
             children: [
               TileLayer(
                 urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
