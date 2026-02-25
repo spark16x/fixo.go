ef0e0ec1f3112de784b6fc38c01b1a43fb4f3fba
Spark16x <spark2009971@gmail.com>
2026-02-25 17:04:06 +0530

    update MainActivity.kt

--- app/android/app/src/main/kotlin/com/spark16x/fixogo/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/spark16x/fixogo/MainActivity.kt
@@ -3,13 +3,3 @@ package com.spark16x.fixogo
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity()
-
-class MainActivity: FlutterFragmentActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
-    }
-
-    override fun getBackgroundMode(): FlutterActivityLaunchConfigs.BackgroundMode {
-        return FlutterActivityLaunchConfigs.BackgroundMode.transparent
-    }
-}
