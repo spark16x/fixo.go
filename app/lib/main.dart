20c7744b585b7b9c93e8febbc475c3b72f2d075c
Spark16x <spark2009971@gmail.com>
2026-02-25 16:56:26 +0530

    update main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'screens/login.dart';
 import 'screens/home.dart';
