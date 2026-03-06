b36d96b546820c7d3e3bec1f089cba62a27f451a
Spark16x <spark2009971@gmail.com>
2026-03-06 14:19:46 +0530

    update

--- app/shared_packages/fixogo_core/lib/fixogo_core.dart
+++
@@ -1,6 +0,0 @@
-library fixogo_core;
-
-export 'src/constants/firestore_paths.dart';
-export 'src/enums/request_status.dart';
-export 'src/enums/user_role.dart';
-export 'src/models/service_request.dart';
--- app/shared_packages/fixogo_core/lib/src/constants/firestore_paths.dart
+++
@@ -1,7 +0,0 @@
-class FirestorePaths {
-  static const users = 'users';
-  static const serviceRequests = 'service_requests';
-
-  static String quotes(String requestId) =>
-      'service_requests/$requestId/quotes';
-}
