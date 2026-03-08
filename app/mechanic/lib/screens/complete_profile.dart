08d9fb4900442d4f72f9cde6f27ae80b7b0ab6e2
Pratham vig <spark2009971@gmail.com>
2026-03-08 18:17:38 +0530

    Use go_router navigation flows in user and mechanic apps

--- app/mechanic/lib/screens/complete_profile.dart
+++ app/mechanic/lib/screens/complete_profile.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:go_router/go_router.dart';
 
-import 'home.dart';
 import '../models/user_model.dart';
 
 class CompleteProfileScreen extends StatefulWidget {
@@ -66,13 +66,7 @@ class _CompleteProfileScreenState
 
       if (!mounted) return;
 
-      Navigator.pushAndRemoveUntil(
-        context,
-        MaterialPageRoute(
-          builder: (_) => const HomeScreen(),
-        ),
-        (route) => false,
-      );
+      context.go('/home');
 
     } catch (e) {
 
---
+++ app/user/lib/core/theme/app_theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static ThemeData lightFromScheme(ColorScheme scheme) {
+    return ThemeData(
+      useMaterial3: true,
+      colorScheme: scheme,
+      brightness: Brightness.light,
+    );
+  }
+
+  static ThemeData darkFromScheme(ColorScheme scheme) {
+    return ThemeData(
+      useMaterial3: true,
+      colorScheme: scheme,
+      brightness: Brightness.dark,
+    );
+  }
+
+  static ThemeData lightFallback() {
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: const Color(0xFF2E7D32),
+      brightness: Brightness.light,
+    );
+  }
+
+  static ThemeData darkFallback() {
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: const Color(0xFF2E7D32),
+      brightness: Brightness.dark,
+    );
+  }
+}
