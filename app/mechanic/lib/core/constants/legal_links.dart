45125c978a895421740273458d4a53f0ea6a5a4d
Spark16x <spark2009971@gmail.com>
2026-03-08 18:05:41 +0530

    update

--- app/mechanic/lib/core/constants/legal_links.dart
+++
@@ -1,4 +0,0 @@
-class LegalLinks {
-  static const terms = 'https://fixo-go.com/terms';
-  static const privacy = 'https://fixo-go.com/privacy';
-}
--- app/user/lib/core/constants/firestore_paths.dart
+++
@@ -1,6 +0,0 @@
-class FirestorePaths {
-  static const users = 'users';
-  static const serviceRequests = 'service_requests';
-
-  static String quotes(String requestId) => '$serviceRequests/$requestId/quotes';
-}
